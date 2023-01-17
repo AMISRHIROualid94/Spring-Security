6e59d31f76902ae4b0d2af8e9f71d8d75a792709
AMISRHIROualid94 <56658551+AMISRHIROualid94@users.noreply.github.com>
2023-01-17 19:05:27 +0100

    exclude h2-console from WebSecurityFilter

--- src/main/java/com/springframwork/springSecurity/config/SecurityConfig.java
+++ src/main/java/com/springframwork/springSecurity/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.springframwork.springSecurity.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,8 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,23 +24,31 @@ import java.util.List;
 public class SecurityConfig{
 
 
-
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() throws Exception{
+        return web -> {
+            web.ignoring().requestMatchers(PathRequest.toH2Console());
+        };
+    }
 
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
-        return http.authorizeHttpRequests()
+      return   http
+                .authorizeHttpRequests()
                         .requestMatchers("/","/**")
                         .permitAll()
-                        .and()
+              .and()
                         .formLogin()
                         .loginPage("/login")
                         .usernameParameter("username")
                         .passwordParameter("password")
                         .defaultSuccessUrl("/welcome",true)
-                        .and()
+              .and()
                         .build();
+
+
     }
     @Bean
     public PasswordEncoder passwordEncoder(){
