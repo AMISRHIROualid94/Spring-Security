0f900f5d54495587a342d9d607083e5064d00cd5
AMISRHIROualid94 <56658551+AMISRHIROualid94@users.noreply.github.com>
2023-01-18 14:18:29 +0100

    Oauth2 connection

--- src/main/java/com/springframwork/springSecurity/config/SecurityConfig.java
+++ src/main/java/com/springframwork/springSecurity/config/SecurityConfig.java
@@ -8,20 +8,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class SecurityConfig{
@@ -34,22 +27,25 @@ public class SecurityConfig{
         };
     }
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
       return   http
-                .authorizeHttpRequests()
-              .requestMatchers("/welcome").hasRole("USER")
-              .requestMatchers("/","/**")
-                        .permitAll()
+                    .authorizeHttpRequests()
+                    .requestMatchers("/welcome").hasRole("USER")
+                    .requestMatchers("/","/**")
+                    .permitAll()
               .and()
-                        .formLogin()
-                        .loginPage("/login")
-                        .usernameParameter("username")
-                        .passwordParameter("password")
-                        .defaultSuccessUrl("/welcome",true)
+                    .formLogin()
+                    .loginPage("/login")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .defaultSuccessUrl("/welcome",true)
+              //.and()
+              //.oauth2Login()
               .and()
-                        .build();
+                    .build();
 
 
     }
