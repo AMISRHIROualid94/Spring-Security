9f30849516d2a912d303bc5b86441f28a3f09667
AMISRHIROualid94 <56658551+AMISRHIROualid94@users.noreply.github.com>
2023-02-05 16:41:28 +0100

    last commit

--- src/main/java/com/springframwork/springSecurity/models/User.java
+++ src/main/java/com/springframwork/springSecurity/models/User.java
@@ -16,7 +16,7 @@ import java.util.Collection;
 @RequiredArgsConstructor
 @Table(name = "users")
 public class User implements UserDetails {
-    @jakarta.persistence.Id
+    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long Id;
     private final String username;
